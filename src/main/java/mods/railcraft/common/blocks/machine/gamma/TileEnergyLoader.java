025beed730e5f10615d978d20e1a3c5579ff503b
__felix__ <134875831+felixfour@users.noreply.github.com>
2025-04-22 13:09:28 -0400

    update modid

--- src/main/java/mods/railcraft/common/blocks/machine/gamma/TileEnergyLoader.java
+++ src/main/java/mods/railcraft/common/blocks/machine/gamma/TileEnergyLoader.java
@@ -31,7 +31,7 @@ import mods.railcraft.common.util.network.IGuiReturnHandler;
 @Optional.InterfaceList(
         value = { @Optional.Interface(
                 iface = "gregtech.api.interfaces.tileentity.IEnergyConnected",
-                modid = "dreamcraft"), })
+                modid = "gregtechNH"), })
 public class TileEnergyLoader extends TileLoaderEnergyBase
         implements ISinkDelegate, IGuiReturnHandler, IEnergyConnected {
 
